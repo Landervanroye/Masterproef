b9907941d52552483ba6738ea4758c7bd2d80ced
Lander Vanroye <vanroyelander@gmail.com>
2019-04-02 22:03:42 +0200

    floor vergeten

--- julia/experiments3.jl
+++ julia/experiments3.jl
@@ -21,7 +21,7 @@ gradsave = zeros(50,10);
 
 for i in 1:length(deltatlist)
     print(i,"   t = ",  deltatlist[i] , "\n");
-    deltat= 1/deltatlist[i];
+    deltat= 1/floor(deltatlist[i]);
     T = Array(0:deltat:1);
     MC_discr = MC_discr_obj(10^6, deltax, xdiscr.x, Array(range(0,stop=L, length = N+1)), xdiscr.N)
     for j = 1:50
