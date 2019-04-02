f2d27d8152af6147f5b4803e03249e0f8a72e08c
Lander Vanroye <vanroyelander@gmail.com>
2019-04-02 21:53:39 +0200

    vor de zekerheid

--- julia/experiments2.jl
+++ julia/experiments2.jl
@@ -38,31 +38,3 @@ for i in 1:length(plist)
     write(file, "gradsave", gradsave)
     close(file)
 end
-
-
-
-
-
-v = range(1,stop=3,length=20);
-deltatlist = [10^(i) for i in v];
-gradsave = zeros(50,10)
-
-for i in 1:length(deltatlist)
-    print(i, ,"   t = ",  deltatlist[i], , "\n")
-    deltat= 1/deltatlist[i];
-    T = Array(0:deltat:1)
-    MC_discr = MC_discr_obj(10^6, deltax, xdiscr.x, Array(range(0,stop=L, length = N+1)), xdiscr.N)
-    for j = 1:50
-        samples_beg, weights_beg =init_MC(problem,MC_discr);
-
-        rng = MersenneTwister(1234+100*i+j);
-        Uout_MC2, samples_end, weights_end= simulate_MC_rng(T,deltat,problem,debdiscr, MC_discr,db, rng,samples_beg, weights_beg);
-
-        rng = MersenneTwister(1234+100*i+j);
-        grad= simulate_adjoint_MC_rng_alt(T,Uout_MC2,db,samples_beg, weights_beg, rng,MC_discr.deltax,debdiscr.deltax,problem.nu, problem, debdiscr, MC_discr);
-        gradsave[i,:] = grad;
-    end
-    file = matopen(string("exp2res/gradt", i, ".mat"), "w")
-    write(file, "gradsave", gradsave)
-    close(file)
-end
--- julia/experiments3.jl
+++ julia/experiments3.jl
@@ -15,34 +15,6 @@ debdiscr = debdiscr_obj(deltaxd, Nd, Array(range(deltaxd/2,stop = (L-deltaxd/2),
 xdiscr = xdiscr_obj(deltax, N, Array(range(deltax/2, stop = L-deltax/2, length = N)));
 
 
-v = range(2,stop=7,length=50);
-plist = [10^(i) for i in v];
-gradsave = zeros(50,10)
-for i in 1:length(plist)
-    print(i,"   p = ", plist[i], "\n")
-    plisti = plist[i]
-    deltat= 0.01;
-    T = Array(0:0.01:1)
-    MC_discr = MC_discr_obj(Int64(floor(plisti)), deltax, xdiscr.x, Array(range(0,stop=L, length = N+1)), xdiscr.N)
-    for j = 1:50
-        samples_beg, weights_beg =init_MC(problem,MC_discr);
-
-        rng = MersenneTwister(1234+100*i+j);
-        Uout_MC2, samples_end, weights_end= simulate_MC_rng(T,deltat,problem,debdiscr, MC_discr,db, rng,samples_beg, weights_beg);
-
-        rng = MersenneTwister(1234+100*i+j);
-        grad= simulate_adjoint_MC_rng_alt(T,Uout_MC2,db,samples_beg, weights_beg, rng,MC_discr.deltax,debdiscr.deltax,problem.nu, problem, debdiscr, MC_discr);
-        gradsave[i,:] = grad;
-    end
-    file = matopen(string("exp2res/gradp", i, ".mat"), "w")
-    write(file, "gradsave", gradsave)
-    close(file)
-end
-
-
-
-
-
 v = range(1,stop=3,length=20);
 deltatlist = [10^(i) for i in v];
 gradsave = zeros(50,10)
@@ -62,7 +34,7 @@ for i in 1:length(deltatlist)
         grad= simulate_adjoint_MC_rng_alt(T,Uout_MC2,db,samples_beg, weights_beg, rng,MC_discr.deltax,debdiscr.deltax,problem.nu, problem, debdiscr, MC_discr);
         gradsave[i,:] = grad;
     end
-    file = matopen(string("exp2res/gradt", i, ".mat"), "w")
+    file = matopen(string("exp3res/gradt", i, ".mat"), "w")
     write(file, "gradsave", gradsave)
     close(file)
 end
