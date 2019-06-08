1f1139cf66714ed836ffc333ba9d5f5f33288d24
root <root@masterproef2.europe-west1-b.c.unique-hour-234914.internal>
2019-06-08 17:35:56 +0000

    experiments28.jl

--- julia2/experiments28.jl
+++ julia2/experiments28.jl
@@ -26,7 +26,7 @@ for i = 1:50
     lrlist[i] = lr;
 
     lr = lr*1.00^(1.0/2.0);
-    Np = ceil(Np*(1.0)^(3.0/2.0));
+    #Np = ceil(Np*(1.0)^(3.0/2.0));
     Np = ceil(Np*(1.3));
 end
 
@@ -39,14 +39,14 @@ include("core.jl")
 #wat correctheidstesten en snelheidstesten
 
 
-function f(d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg)
+function f(T,xdiscr, d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg)
     rng = MersenneTwister(seed);
     Uout, samples_end, weights_end= simulate_MC_rng(T,deltat,problem,debdiscr, MC_discr,d, rng,samples_beg, weights_beg);
     result = evalF(Uout, d, xdiscr, debdiscr, problem, T);
     return result
 end
 
-function g!(G, d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg)
+function g!(T,xdiscr,G, d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg)
     rng = MersenneTwister(seed);
     Uout_MC, samples_end, weights_end= simulate_MC_rng(T,deltat,problem,debdiscr, MC_discr,d, rng,samples_beg, weights_beg);
     rng = MersenneTwister(seed);
@@ -60,29 +60,30 @@ for i in 1:length(plist)
     print(i,"   p = ", plist[i], "\n")
     plisti = plist[i];
     Nt= Nlist[i];
-    N = Nblist[i];
+    N = Int64(Nblist[i]);
+    Nd = 10;
 
     L = 1.0;
     deltax = L/N;
     alpha = 0.01;
     nu = Float64(0.5);
     deltat= 1.0/Nt;
-    T = Array(0:deltat:1)
-    Nd = 10;
+    T = Array(0:deltat:1);
     deltaxd = L/Nd;
     db = Array(range(0.0, stop=0.1, length=Nd));
     xdiscrx = Array(range(deltax/2,stop = (L-deltax/2),length=N));
     BEGINVWDN = cos.((xdiscrx*2*pi/L)).+1.1;
     problem = problem_obj(nu, alpha, BEGINVWDN);
-    debdiscr = debdiscr_obj(deltaxd, Nd, Array(range(deltaxd/2,stop = (L-deltaxd/2),length=Nd)));
+    debdiscr = debdiscr_obj(deltaxd, 10, Array(range(deltaxd/2,stop = (L-deltaxd/2),length=Nd)));
     xdiscr = xdiscr_obj(deltax, N, Array(range(deltax/2, stop = L-deltax/2, length = N)));
+    MC_discr = MC_discr_obj(Int64(floor(plisti)), deltax, xdiscr.x, Array(range(0,stop=L, length = N+1)), xdiscr.N)
 
 
 
     for j = 1:50
         samples_beg, weights_beg =init_MC(problem,MC_discr);
         seed = 1234+100*i+j;
-        optimres = optimize(d-> f(d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg), (G,d) -> g!(G, d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg), db, ConjugateGradient(), Optim.Options(show_trace =false));
+        optimres = optimize(d-> f(T,xdiscr,d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg), (G,d) -> g!(T,xdiscr,G, d, seed, deltat, problem, debdiscr, MC_discr, samples_beg, weights_beg), db, ConjugateGradient(), Optim.Options(show_trace =false));
         optimsave[j,:] = optimres.minimizer;
     end
     file = matopen(string("exp28res/b_", "optim", i, ".mat"), "w")
